82d26aded46077c7cc0a04042bebbe7a674bbcf6
zeurjr <elizeuribeiroo@gmail.com>
2021-03-25 22:41:05 -0300

    Inicio do projeto

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: MyHomePage()
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Despesas Pessoais'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.green,
+              child: Text('Grafico'),
+              elevation:5,
+            ),
+          ),
+          Card(
+            child: Text('Lista de Transações'),
+          )
+        ],
+      ),
+    );
+  }
+}
+
