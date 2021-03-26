acbee5857bc515989a06ec1518414588465c9650
zeurjr <elizeuribeiroo@gmail.com>
2021-03-26 19:16:24 -0300

    Conversão de elementos visuais

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 main() => runApp(ExpensesApp());
@@ -5,13 +6,25 @@ main() => runApp(ExpensesApp());
 class ExpensesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: MyHomePage()
-    );
+    return MaterialApp(home: MyHomePage());
   }
 }
 
 class MyHomePage extends StatelessWidget {
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo Tenis de corrida',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta de luz',
+      value: 211.30,
+      date: DateTime.now(),
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -28,15 +41,17 @@ class MyHomePage extends StatelessWidget {
             child: Card(
               color: Colors.green,
               child: Text('Grafico'),
-              elevation:5,
+              elevation: 5,
             ),
           ),
-          Card(
-            child: Text('Lista de Transações'),
+          Column(children: _transactions.map((tr) {
+            return Card(
+              child: Text(tr.title),
+            );
+          }).toList(),
           )
         ],
       ),
     );
   }
 }
-
